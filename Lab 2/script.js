075c0dc362bce27d6adcdbf7a7616a68cd54510d
Joakim Råman <joakim.raman@gmail.com>
2024-01-03 20:05:07 +0100

    Added name and e-mail fields with error checks.

---
+++ Lab 2/script.js
@@ -0,0 +1,37 @@
+function containsNonLetters(myString) {
+    str = myString.toLowerCase();
+
+    for (let i = 0; i < str.length; i++) {
+        char = str[i];
+        if (char < 'a' || char > 'z') {
+            if (char !== 'å' && char !== 'ä' && char !== 'ö')
+                return true;
+        }
+    }
+    return false;
+}
+
+const form = document.getElementById('quiz-form');
+const firstname = document.getElementById('first-name-field');
+const lastname = document.getElementById('last-name-field');
+const email = document.getElementById('email-field');
+const nameError = document.getElementById('name-error-message');
+const emailError = document.getElementById('email-error-message');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    if (!email.value.includes('@') || !email.value.includes('.')) {
+        emailError.textContent = "You need to provide a proper e-mail address!";
+    }
+    else {
+        emailError.textContent = "";
+    }
+
+    if (containsNonLetters(firstname.value)) {
+        nameError.textContent = "Only letters (a-ö) are allowed for the first and last names!";
+    }
+    else {
+        nameError.textContent = "";
+    }
+});
