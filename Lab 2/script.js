af72bb4a96071c6d0052d4b6f40dae5b77068ff2
Joakim Råman <joakim.raman@gmail.com>
2024-01-04 14:36:41 +0100

    Completed all five questions, improved design. Started coding for checking for correct answers.

--- Lab 2/script.js
+++ Lab 2/script.js
@@ -18,6 +18,17 @@ const email = document.getElementById('email-field');
 const nameError = document.getElementById('name-error-message');
 const emailError = document.getElementById('email-error-message');
 
+const question11 = document.getElementById('question-1-1');
+const question12 = document.getElementById('question-1-2');
+const question13 = document.getElementById('question-1-3');
+const question14 = document.getElementById('question-1-4');
+
+const question1 = {type: 'radio', numOfAlternatives: 4, correctAnswers: [0]};
+const question2 = {type: 'check', numOfAlternatives: 6, correctAnswers: [0,2,4]};
+const question3 = {type: 'text', numOfAlternatives: 0, correctAnswers: ["tetris"]};
+const question4 = {type: 'radio', numOfAlternatives: 4, correctAnswers: [2]};
+const question5 = {type: 'check', numOfAlternatives: 4, correctAnswers: [0,3,4]};
+
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -34,4 +45,9 @@ form.addEventListener('submit', function(event) {
     else {
         nameError.textContent = "";
     }
+
+    console.log('A: '+question11.checked);
+    console.log('B: '+question12.checked);
+    console.log('C: '+question13.checked);
+    console.log('D: '+question14.checked);
 });
