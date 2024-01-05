2389725f175af0e7529b49befcc6990c446950e8
Joakim Råman <joakim.raman@gmail.com>
2024-01-05 15:41:40 +0100

    Implemented scoring in JS which is printed in the console.

--- Lab 2/script.js
+++ Lab 2/script.js
@@ -28,6 +28,7 @@ const question2 = {type: 'check', numOfAlternatives: 6, correctAnswers: [0,2,4]}
 const question3 = {type: 'text', numOfAlternatives: 0, correctAnswers: ["tetris"]};
 const question4 = {type: 'radio', numOfAlternatives: 4, correctAnswers: [2]};
 const question5 = {type: 'check', numOfAlternatives: 4, correctAnswers: [0,3,4]};
+const questions = [question1, question2, question3, question4, question5];
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -46,8 +47,44 @@ form.addEventListener('submit', function(event) {
         nameError.textContent = "";
     }
 
-    console.log('A: '+question11.checked);
-    console.log('B: '+question12.checked);
-    console.log('C: '+question13.checked);
-    console.log('D: '+question14.checked);
+    let score = 0;
+
+    for (let i=0; i<questions.length; i++) {
+        switch (questions[i].type) {
+            case 'radio':
+                console.log('Question '+i+' is of type radio.');
+                for (let j=0; j<questions[i].numOfAlternatives; j++) {
+                    let answerString = 'question-'+i+'-'+j;
+                    let answer = document.getElementById(answerString).checked;
+                    if (answer === true && questions[i].correctAnswers[0] === j) {
+                        score++;
+                        break;
+                    }
+                }
+                break;
+            case 'check':
+                console.log('Question '+i+' is of type check.');
+                for (let j=0; j<questions[i].numOfAlternatives; j++) {
+                    let answerString = 'question-'+i+'-'+j;
+                    let answer = document.getElementById(answerString).checked;
+                    if (answer === true) {
+                        if (questions[i].correctAnswers.includes(j))
+                            score+=0.25;
+                        else
+                            score-=0.25;
+                    }
+                }
+                break;
+            case 'text':
+                console.log('Question '+i+' is of type text.');
+                let answerString = 'question-'+i;
+                let answer = document.getElementById(answerString).value;
+                console.log('Answer: '+answer.toLowerCase());
+                console.log('Correct answer: '+questions[i].correctAnswers[0]);
+                if (answer.toLowerCase() === questions[i].correctAnswers[0])
+                    score+=1;
+        }
+    }
+
+    console.log('Score: '+score);
 });
