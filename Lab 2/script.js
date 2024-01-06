4791cdabcb1ac29b4cad7bebbe94e12b5948154d
Joakim Råman <joakim.raman@gmail.com>
2024-01-07 00:22:56 +0100

    Implemented loading of saved quizzes in JS, preparing for quiz creation.

--- Lab 2/script.js
+++ Lab 2/script.js
@@ -11,24 +11,102 @@ function containsNonLetters(myString) {
     return false;
 }
 
-const form = document.getElementById('quiz-form');
-const firstname = document.getElementById('first-name-field');
-const lastname = document.getElementById('last-name-field');
-const email = document.getElementById('email-field');
-const nameError = document.getElementById('name-error-message');
-const emailError = document.getElementById('email-error-message');
+const quizSelectionForm = document.getElementById('quiz-selection-form');
+const quizChoice = document.getElementById('quizzes');
+const fullQuizContainer = document.getElementById('full-quiz');
 
-const question11 = document.getElementById('question-1-1');
-const question12 = document.getElementById('question-1-2');
-const question13 = document.getElementById('question-1-3');
-const question14 = document.getElementById('question-1-4');
 
-const question1 = {type: 'radio', numOfAlternatives: 4, correctAnswers: [0]};
-const question2 = {type: 'check', numOfAlternatives: 6, correctAnswers: [0,2,4]};
-const question3 = {type: 'text', numOfAlternatives: 0, correctAnswers: ["tetris"]};
-const question4 = {type: 'radio', numOfAlternatives: 4, correctAnswers: [2]};
-const question5 = {type: 'check', numOfAlternatives: 4, correctAnswers: [0,3,4]};
-const questions = [question1, question2, question3, question4, question5];
+// Quiz creation:
+const question1 = {type: 'radio', numOfAlternatives: 4, correctAnswers: [0], required: true, question: 'What is the capital of Azerbaijan?', alternatives: ['Baku', 'Kayseri', 'Jerevan', 'Tblisi']};
+const question2 = {type: 'check', numOfAlternatives: 6, correctAnswers: [0,2,4], required: true, question: 'Which of the following are places in Sweden?', alternatives: ['Vilhelmina', 'Trofors', 'Puoltikasvaara', 'Kristiansand', 'Dorotea', 'Uleåborg']};
+const question3 = {type: 'text', numOfAlternatives: 0, correctAnswers: ['tetris'], required: false, question: 'What is the name of the classic 1980s video game based on falling blocks created by a Soviet programmer?', alternatives: []};
+const question4 = {type: 'radio', numOfAlternatives: 4, correctAnswers: [2], required: false, question: 'The city of Kristianstad was founded in 1614 by which Danish king?', alternatives: ['Frederick II', 'Christian III', 'Christian IV', 'Christian V']};
+const question5 = {type: 'check', numOfAlternatives: 6, correctAnswers: [0,3,4], required: false, question: 'Who of the following are famous physicists?', alternatives: ['Isaac Newton', 'Franz Abt', 'Rudolf Bultmann', 'James Clerk Maxwell', 'Wolfgang Pauli', 'Hans Cornelius']};
+const qs = [question1, question2, question3, question4, question5];
+localStorage.setItem('Default', JSON.stringify(qs));
+
+//Loading of the quiz list:
+let myQuizList = ['Default'];
+let savedQuizList = JSON.parse(localStorage.getItem('Quiz list'));
+myQuizList.concat(savedQuizList);
+let quizMenuHTML = '';
+for (let i=0; i<myQuizList.length; i++) {
+    quizMenuHTML += `<option value="${myQuizList[i]}">${myQuizList[i]}</option>`
+}
+document.getElementById('quizzes').innerHTML = quizMenuHTML; 
+
+
+//Loading of the chosen quiz:
+quizSelectionForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    fullQuizContainer.innerHTML = `
+    <h3>Quiz: ${quizChoice.value}</h3>
+    <input type="text" class="contact-field" name="first-name" id ="first-name-field" placeholder="First name" required>
+    <input type="text" class="contact-field" name="last-name" id="last-name-field" placeholder="Last name" required>
+    <input type="text" class="contact-field" name="email" id="email-field" placeholder="E-mail" required>
+    <form class="quiz-question-set" id="quiz-form">
+        <div id="quiz-question-container"></div>
+        <button type="submit" class="submit-button">SUBMIT</button>
+    </form>
+    <p id="name-error-message"></p>
+    <p id="email-error-message"></p>
+    <p>* This question must be answered.</p>`;
+
+    let questions = JSON.parse(localStorage.getItem(quizChoice.value));
+    let questionsHTML = '';
+    
+    const form = document.getElementById('quiz-form');
+    const firstname = document.getElementById('first-name-field');
+    const lastname = document.getElementById('last-name-field');
+    const email = document.getElementById('email-field');
+    const nameError = document.getElementById('name-error-message');
+    const emailError = document.getElementById('email-error-message');
+
+    for (let i=0; i<questions.length; i++) {
+        console.log(i);
+        switch(questions[i].type) {
+            case 'radio':
+                questionsHTML += `
+                <div class="quiz-question">
+                    <p class="quiz-question-text">${questions[i].question}</p>`;
+                for (let j=0; j<questions[i].alternatives.length; j++) {
+                    questionsHTML += `
+                    <div class="quiz-radio-alternative-set">
+                        <input type="radio" id="question-${i}-${j}" name="question-${i}" value=${questions[i].alternatives[j]}>
+                        <label for="question-${i}-${j}" class="quiz-question-alternative-text">${questions[i].alternatives[j]}</label>
+                    </div>`;
+                }
+                questionsHTML += '</div>';    
+                break;
+            case 'check':
+                questionsHTML += `
+                <div class="quiz-question">
+                    <p class="quiz-question-text">${questions[i].question}</p>`;
+                for (let j=0; j<questions[i].alternatives.length; j++) {
+                    questionsHTML += `
+                    <div class="quiz-radio-alternative-set">
+                        <input type="checkbox" id="question-${i}-${j}" name="question-${i}-${j}" value=${questions[i].alternatives[j]}>
+                        <label for="question-${i}-${j}" class="quiz-question-alternative-text">${questions[i].alternatives[j]}</label>
+                    </div>`;
+                }
+                questionsHTML += '</div>';    
+                break;
+            case 'text':
+                questionsHTML += `
+                <div class="quiz-question">
+                    <p class="quiz-question-text">${questions[i].question}</p>
+                    <div class="quiz-radio-alternative-set">
+                        <input type="text" id="question-${i}" name="question-${i}" class="quiz-text-input">
+                    </div>
+                </div>`
+                break;            
+        }
+    }
+    document.getElementById('quiz-question-container').innerHTML = questionsHTML;
+});
+
+
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -56,6 +134,7 @@ form.addEventListener('submit', function(event) {
                 for (let j=0; j<questions[i].numOfAlternatives; j++) {
                     let answerString = 'question-'+i+'-'+j;
                     let answer = document.getElementById(answerString).checked;
+                    console.log('Answer: '+answer);
                     if (answer === true && questions[i].correctAnswers[0] === j) {
                         score++;
                         break;
